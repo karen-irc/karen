ace1359afc29eadeea92e5c3e6ea2a6fa1a69371
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 04:36:12 +0900

    Lint fix: invite.js

--- src/plugins/inputs/invite.js
+++ src/plugins/inputs/invite.js
@@ -1,5 +1,8 @@
+/*eslint quotes: [2, "single"]*/
+'use strict';
+
 module.exports = function(network, chan, cmd, args) {
-    if (cmd != "invite") {
+    if (cmd !== 'invite') {
         return;
     }
     var irc = network.irc;
