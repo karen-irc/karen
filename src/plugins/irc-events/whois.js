90c6a0b0582a5c107c1f4eb6f3b8f3fb88760cad
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 04:44:10 +0900

    Lint fix whois.js

--- src/plugins/irc-events/whois.js
+++ src/plugins/irc-events/whois.js
@@ -1,46 +1,52 @@
-var _ = require("lodash");
-var Chan = require("../../models/Channel");
+'use strict';
+
+var _ = require('lodash');
+var Chan = require('../../models/Channel');
 var ChannelType = require('../../models/ChannelType');
-var Msg = require("../../models/Message");
+var Msg = require('../../models/Message');
 var MessageType = require('../../models/MessageType');
 
 module.exports = function(irc, network) {
     var client = this;
-    irc.on("whois", function(err, data) {
+    irc.on('whois', function(err, data) {
+        if (!!err) {
+            throw err;
+        }
+
         if (data === null) {
             return;
         }
         var chan = _.findWhere(network.channels, {name: data.nickname});
-        if (typeof chan === "undefined") {
+        if (typeof chan === 'undefined') {
             chan = new Chan({
                 type: ChannelType.QUERY,
                 name: data.nickname
             });
             network.channels.push(chan);
-            client.emit("join", {
+            client.emit('join', {
                 network: network.id,
                 chan: chan
             });
         }
         var prefix = {
-            hostname: "from",
-            realname: "is",
-            channels: "on",
-            server: "using"
+            hostname: 'from',
+            realname: 'is',
+            channels: 'on',
+            server: 'using'
         };
         var i = 0;
         for (var k in data) {
             var key = prefix[k];
-            if (!key || data[k].toString() === "") {
+            if (!key || data[k].toString() === '') {
                 continue;
             }
             var msg = new Msg({
                 type: MessageType.WHOIS,
                 from: data.nickname,
-                text: key + " " + data[k]
+                text: key + ' ' + data[k]
             });
             chan.messages.push(msg);
-            client.emit("msg", {
+            client.emit('msg', {
                 chan: chan.id,
                 msg: msg
             });
