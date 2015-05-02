c9f3b353e401b79339df21f3eb6adb5aa8fd1331
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 00:42:59 +0900

    Replace double quote to single quote

--- src/models/Network.js
+++ src/models/Network.js
@@ -1,3 +1,4 @@
+/*eslint quotes: [2, "single"]*/
 /**
  * @license MIT License
  *
@@ -24,23 +25,23 @@
  */
 'use strict';
 
-const _ = require("lodash");
-const assign = require("object-assign");
-const Chan = require("./Channel");
+const _ = require('lodash');
+const assign = require('object-assign');
+const Chan = require('./Channel');
 const ChannelType = require('./ChannelType');
 
 let id = 0;
 
 function Network(attr) {
     _.merge(this, assign({
-        name: "",
-        host: "",
+        name: '',
+        host: '',
         port: 6667,
         tls: false,
-        password: "",
+        password: '',
         commands: [],
-        username: "",
-        realname: "",
+        username: '',
+        realname: '',
         channels: [],
         connected: false,
         id: id++,
@@ -56,31 +57,31 @@ function Network(attr) {
 }
 
 Network.prototype.toJSON = function() {
-    let json = assign(this, {nick: (this.irc || {}).me || ""});
-    return _.omit(json, "irc", "password");
+    let json = assign(this, {nick: (this.irc || {}).me || ''});
+    return _.omit(json, 'irc', 'password');
 };
 
 Network.prototype.export = function() {
     let network = _.pick(this, [
-        "name",
-        "host",
-        "port",
-        "tls",
-        "password",
-        "username",
-        "realname",
-        "commands"
+        'name',
+        'host',
+        'port',
+        'tls',
+        'password',
+        'username',
+        'realname',
+        'commands'
     ]);
     network.nick = (this.irc || {}).me;
     network.join = _.pluck(
-        _.where(this.channels, {type: "channel"}),
-        "name"
-    ).join(",");
+        _.where(this.channels, {type: 'channel'}),
+        'name'
+    ).join(',');
     return network;
 };
 
 function prettify(host) {
-    let name = capitalize(host.split(".")[1]);
+    let name = capitalize(host.split('.')[1]);
     if (!name) {
         name = host;
     }
@@ -88,7 +89,7 @@ function prettify(host) {
 }
 
 function capitalize(str) {
-    if (typeof str === "string") {
+    if (typeof str === 'string') {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 }
