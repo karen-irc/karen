fab8180e89807bf4d7126302879e20df57295b5d
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 04:26:04 +0900

    Lint fix: services.js

--- src/plugins/inputs/services.js
+++ src/plugins/inputs/services.js
@@ -1,23 +1,26 @@
-var _ = require("lodash");
+/*eslint quotes: [2, "single"]*/
+'use strict';
+
+var _ = require('lodash');
 
 module.exports = function(network, chan, cmd, args) {
-    if (cmd != "ns" && cmd != "cs" && cmd != "hs") {
+    if (cmd !== 'ns' && cmd !== 'cs' && cmd !== 'hs') {
         return;
     }
     var target = ({
-        "ns": "nickserv",
-        "cs": "chanserv",
-        "hs": "hostserv",
+        'ns': 'nickserv',
+        'cs': 'chanserv',
+        'hs': 'hostserv',
     })[cmd];
-    if (!target || args.length === 0 || args[0] === "") {
+    if (!target || args.length === 0 || args[0] === '') {
         return;
     }
     var irc = network.irc;
-    var msg = args.join(" ");
+    var msg = args.join(' ');
     irc.send(target, msg);
     var channel = _.find(network.channels, {name: target});
-    if (typeof channel !== "undefined") {
-        irc.emit("message", {
+    if (typeof channel !== 'undefined') {
+        irc.emit('message', {
             from: irc.me,
             to: channel.name,
             message: msg
