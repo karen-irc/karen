83e1543614bfd8f32b6bded130742acd06ad3e45
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 04:46:22 +0900

    Lint fix notice.js

--- src/plugins/irc-events/notice.js
+++ src/plugins/irc-events/notice.js
@@ -1,23 +1,25 @@
-var _ = require("lodash");
-var Msg = require("../../models/Message");
+'use strict';
+
+var _ = require('lodash');
+var Msg = require('../../models/Message');
 var MessageType = require('../../models/MessageType');
 
 module.exports = function(irc, network) {
     var client = this;
-    irc.on("notice", function(data) {
+    irc.on('notice', function(data) {
         var target = data.to;
-        if (target.toLowerCase() == irc.me.toLowerCase()) {
+        if (target.toLowerCase() === irc.me.toLowerCase()) {
             target = data.from;
         }
 
         var chan = _.findWhere(network.channels, {name: target});
-        if (typeof chan === "undefined") {
+        if (typeof chan === 'undefined') {
             chan = network.channels[0];
         }
 
-        var from = data.from || "";
-        if (data.to == "*" || data.from.indexOf(".") !== -1) {
-            from = "";
+        var from = data.from || '';
+        if (data.to === '*' || data.from.indexOf('.') !== -1) {
+            from = '';
         }
         var msg = new Msg({
             type: MessageType.NOTICE,
@@ -25,7 +27,7 @@ module.exports = function(irc, network) {
             text: data.message
         });
         chan.messages.push(msg);
-        client.emit("msg", {
+        client.emit('msg', {
             chan: chan.id,
             msg: msg
         });
