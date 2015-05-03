6df273aba28a0c0e76f7bfbfe638a7122d0906f2
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-05-03 09:25:57 +0900

    ES6fy src/clientManager.js

--- src/clientManager.js
+++ src/clientManager.js
@@ -5,178 +5,174 @@ import mkdirp from 'mkdirp';
 import Helper from './helper';
 import moment from 'moment';
 
-/**
- *  @constructor
- */
-export default function ClientManager() {
-    this.clients = [];
-}
+export default class ClientManager {
 
-/**
- *  @param  {string} name
- *  @return {Client}
- */
-ClientManager.prototype._findClient = function(name) {
-    for (var i in this.clients) {
-        var client = this.clients[i];
-        if (client.name === name) {
-            return client;
-        }
-    }
-    return null;
-};
-
-/**
- * @return {void}
- */
-ClientManager.prototype.loadUsers = function() {
-    var users = this.getUsers();
-    for (var i in users) {
-        this.loadUser(users[i]);
-    }
-};
-
-/**
- * @param {string} name
- * @return {void}
- */
-ClientManager.prototype.loadUser = function(name) {
-    var raw = null;
-    try {
-        raw = fs.readFileSync(
-            Helper.HOME + '/users/' + name + '.json',
-            'utf-8'
-        );
-    }
-    catch(e) {
-        console.log(e);
+    /**
+     *  @constructor
+     */
+    constructor() {
+        this.clients = [];
     }
 
-    var json = null;
-    try {
-        json = JSON.parse(raw);
-    } catch(e) {
-        console.log(e);
-    }
-
-    if (json === null) {
-        return;
-    }
-
-    if (this._findClient(name) !== null) {
-        return;
-    }
-
-    var user = new Client(this.sockets, name, json);
-    this.clients.push(user);
-
-    var message = 'User \'' + name + '\' loaded.';
-    console.log(message);
-};
-
-/**
- * @return {Array}
- */
-ClientManager.prototype.getUsers = function() {
-    var users = [];
-    var path = Helper.HOME + '/users';
-    mkdirp.sync(path);
-    try {
-        var files = fs.readdirSync(path);
-        files.forEach(function(file) {
-            if (file.indexOf('.json') !== -1) {
-                users.push(file.replace('.json', ''));
+    /**
+     *  @param  {string} name
+     *  @return {Client}
+     */
+    _findClient(name) {
+        for (const client of this.clients) {
+            if (client.name === name) {
+                return client;
             }
-        });
-    } catch(e) {
-        console.log(e);
+        }
         return null;
     }
-    return users;
-};
 
-/**
- * @param  {string} name
- * @param  {string} password
- * @return {boolean}
- *
- * @throws {Error}
- */
-ClientManager.prototype.addUser = function(name, password) {
-    var users = this.getUsers();
-    if (users.indexOf(name) !== -1) {
-        return false;
+    /**
+     * @return {void}
+     */
+    loadUsers() {
+        let users = this.getUsers();
+        for (const i in users) {
+            this.loadUser(users[i]);
+        }
     }
-    try {
-        var path = Helper.HOME + '/users';
-        var user = {
-            user: name,
-            password: password || '',
-            log: false,
-            networks: []
-        };
-        mkdirp.sync(path);
-        fs.writeFileSync(
-            path + '/' + name + '.json',
-            JSON.stringify(user, null, '  '),
-            {mode: '0777'}
-        );
-    } catch(e) {
-        throw e;
-    }
-    return true;
-};
 
-/**
- * @param  {string} name
- * @return {boolean}
- *
- * @throws {Error}
- */
-ClientManager.prototype.removeUser = function(name) {
-    var users = this.getUsers();
-    if (users.indexOf(name) === -1) {
-        return false;
-    }
-    try {
-        var path = Helper.HOME + '/users/' + name + '.json';
-        fs.unlinkSync(path);
-    } catch(e) {
-        throw e;
-    }
-    return true;
-};
-
-/**
- * @return {void}
- */
-ClientManager.prototype.autoload = function() {
-    var self = this;
-    var loaded = [];
-    setInterval(function() {
-        var loaded = _.pluck(
-            self.clients,
-            'name'
-        );
-        var added = _.difference(self.getUsers(), loaded);
-        _.each(added, function(name) {
-            self.loadUser(name);
-        });
-        var removed = _.difference(loaded, self.getUsers());
-        _.each(removed, function(name) {
-            var client = _.find(
-                self.clients, {
-                    name: name
-                }
+    /**
+     * @param {string} name
+     * @return {void}
+     */
+    loadUser(name) {
+        let raw = null;
+        try {
+            raw = fs.readFileSync(
+                Helper.HOME + '/users/' + name + '.json',
+                'utf-8'
             );
-            if (client) {
-                client.quit();
-                self.clients = _.without(self.clients, client);
-                console.log(
-                    'User \'' + name + '\' disconnected.'
-                );
-            }
-        });
-    }, 1000);
-};
+        }
+        catch(e) {
+            console.log(e);
+        }
 
+        let json = null;
+        try {
+            json = JSON.parse(raw);
+        } catch(e) {
+            console.log(e);
+        }
+
+        if (json === null) {
+            return;
+        }
+
+        if (this._findClient(name) !== null) {
+            return;
+        }
+
+        const user = new Client(this.sockets, name, json);
+        this.clients.push(user);
+
+        const message = 'User \'' + name + '\' loaded.';
+        console.log(message);
+    }
+
+    /**
+     * @return {Array}
+     */
+    getUsers() {
+        const users = [];
+        const path = Helper.HOME + '/users';
+        mkdirp.sync(path);
+        try {
+            const files = fs.readdirSync(path);
+            files.forEach(function(file) {
+                if (file.indexOf('.json') !== -1) {
+                    users.push(file.replace('.json', ''));
+                }
+            });
+        } catch(e) {
+            console.log(e);
+            return null;
+        }
+        return users;
+    }
+
+    /**
+     * @param  {string} name
+     * @param  {string} password
+     * @return {boolean}
+     *
+     * @throws {Error}
+     */
+    addUser(name, password) {
+        const users = this.getUsers();
+        if (users.indexOf(name) !== -1) {
+            return false;
+        }
+
+        try {
+            const path = Helper.HOME + '/users';
+            const user = {
+                user: name,
+                password: password || '',
+                log: false,
+                networks: []
+            };
+            mkdirp.sync(path);
+            fs.writeFileSync(
+                path + '/' + name + '.json',
+                JSON.stringify(user, null, '  '),
+                {mode: '0777'}
+            );
+        } catch(e) {
+            throw e;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param  {string} name
+     * @return {boolean}
+     *
+     * @throws {Error}
+     */
+    removeUser(name) {
+        const users = this.getUsers();
+        if (users.indexOf(name) === -1) {
+            return false;
+        }
+        try {
+            const path = Helper.HOME + '/users/' + name + '.json';
+            fs.unlinkSync(path);
+        } catch(e) {
+            throw e;
+        }
+        return true;
+    }
+
+    /**
+     * @return {void}
+     */
+    autoload() {
+        setInterval(() => {
+            const loaded = _.pluck(this.clients, 'name');
+            const added = _.difference(this.getUsers(), loaded);
+
+            _.each(added, (name) => {
+                this.loadUser(name);
+            });
+
+            const removed = _.difference(loaded, this.getUsers());
+            _.each(removed, (name) => {
+                const client = _.find(this.clients, {
+                    name: name
+                });
+                if (client) {
+                    client.quit();
+                    this.clients = _.without(this.clients, client);
+                    console.log('User \'' + name + '\' disconnected.');
+                }
+            });
+        }, 1000);
+    }
+}
