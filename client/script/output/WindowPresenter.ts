karen-irc/karen

commit 112360adf31446797f30e79f7ed72e7c8e47138f

Make InputBoxViewController TypeScriptify.

client/script/output/view/InputBoxViewController.ts
@@ -22,44 +22,39 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+ 
+ /// <reference path="../../../../tsd/thrid_party/mousetrap/mousetrap.d.ts" />
+ /// <reference path="../../../../node_modules/rx/ts/rx.d.ts" />
 
 import MessageActionCreator from '../../intent/action/MessageActionCreator';
-import Mousetrap from 'mousetrap';
-import Rx from 'rx';
+import * as MousetrapMod from 'mousetrap';
+import * as Rx from 'rx';
 import UIActionCreator from '../../intent/action/UIActionCreator';
+
+// XXX: Mousetrap's type defenitions in Definitelytyped
+// don't support to call as constructor.
+const Mousetrap: any = MousetrapMod;
 
 export default class InputBoxViewController {
 
-    /**
-     *  @constructor
-     *  @param  {DomainState}   domain
-     *  @param  {Element}   element
-     */
-    constructor(domain, element) {
-        if (!element) {
-            throw new Error();
-        }
+    _element: Element;
+    _domain: any;
+    _textInput: HTMLInputElement;
+    _disposeFocus: Rx.IDisposable;
 
-        /** @type   {Element}   */
+    constructor(domain: any, element: Element) {
         this._element = element;
-
-        /** @type   {DomainState}   */
         this._domain = domain;
+        this._textInput = <HTMLInputElement>element.querySelector('#input');
 
-        /** @type   {Element}   */
-        this._textInput = element.querySelector('#input');
-
-        /*eslint-disable valid-jsdoc */
-        /** @type   {Rx.IDisposable}    */
         this._disposeFocus = UIActionCreator.getDispatcher().focusInputBox.subscribe(() => {
             this.focusInput();
         });
-        /*eslint-enable*/
 
         this._init();
     }
 
-    _init() {
+    _init(): void {
         this._element.addEventListener('submit', this);
 
         const shortcut = [
@@ -67,25 +62,18 @@ export default class InputBoxViewController {
             'ctrl+l',
             'ctrl+shift+l'
         ];
-        new Mousetrap(this._textInput).bind(shortcut, (aEvent) => {
+        new Mousetrap(this._textInput).bind(shortcut, (aEvent: Event) => {
             if (aEvent.target === this._textInput) {
                 MessageActionCreator.clear();
             }
         });
     }
 
-    /**
-     *  @return {Element}
-     */
-    get textInput() {
+    get textInput(): HTMLInputElement {
         return this._textInput;
     }
 
-    /**
-     *  @param  {Event} aEvent
-     *  @return {void}
-     */
-    handleEvent(aEvent) {
+    handleEvent(aEvent: Event): void {
         switch (aEvent.type) {
             case 'submit':
                 this.onSubmit(aEvent);
@@ -93,11 +81,7 @@ export default class InputBoxViewController {
         }
     }
 
-    /**
-     *  @param  {Event} aEvent
-     *  @return {void}
-     */
-    onSubmit(aEvent) {
+    onSubmit(aEvent: Event): void {
         aEvent.preventDefault();
 
         const input = this._textInput;
@@ -112,10 +96,7 @@ export default class InputBoxViewController {
         input.readOnly = false;
     }
 
-    /**
-     *  @return {void}
-     */
-    focusInput() {
+    focusInput(): void {
         this._textInput.focus();
     }
 }

commit b7cc43b32ae6d277ad0c580aace33a965529440f

Fix the regression by: 112360adf31446797f30e79f7ed72e7c8e47138f
Mousetrap's distributed d.ts don't support to call as constructor.
This is a quick fix.

client/script/output/WindowPresenter.ts
@@ -24,13 +24,14 @@
  */
 
 /// <reference path="../../../tsd/core-js.d.ts" />
+/// <reference path="../../../tsd/extends.d.ts" />
 /// <reference path="../../../tsd/thrid_party/mousetrap/mousetrap.d.ts" />
 /// <reference path="../../../node_modules/option-t/option-t.d.ts" />
 
 import arrayFindIndex from 'core-js/library/fn/array/find-index';
 import AppActionCreator from '../intent/action/AppActionCreator';
 import Channel from '../model/Channel';
-import * as Mousetrap from 'mousetrap';
+import Mousetrap from 'mousetrap';
 import UIActionCreator from '../intent/action/UIActionCreator';
 import {Option} from 'option-t';
 
@@ -71,13 +72,13 @@ export default class WindowPresenter {
         });
 
         /*eslint-enable*/
-
-        Mousetrap.bind([
+        const mousetrap = new Mousetrap(window.document.documentElement);
+        mousetrap.bind([
             'command+up',
             'command+down',
             'ctrl+up',
             'ctrl+down'
-        ], (e, keys) => {
+        ], (e: Event, keys: string) => {
             this.handleShortcut(keys);
         });
     }

client/script/output/view/InputBoxViewController.ts
@@ -22,18 +22,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
- /// <reference path="../../../../tsd/thrid_party/mousetrap/mousetrap.d.ts" />
- /// <reference path="../../../../node_modules/rx/ts/rx.d.ts" />
+
+/// <reference path="../../../../tsd/extends.d.ts" />
+/// <reference path="../../../../tsd/thrid_party/mousetrap/mousetrap.d.ts" />
+/// <reference path="../../../../node_modules/rx/ts/rx.d.ts" />
 
 import MessageActionCreator from '../../intent/action/MessageActionCreator';
-import * as MousetrapMod from 'mousetrap';
+import Mousetrap from 'mousetrap';
 import * as Rx from 'rx';
 import UIActionCreator from '../../intent/action/UIActionCreator';
-
-// XXX: Mousetrap's type defenitions in Definitelytyped
-// don't support to call as constructor.
-const Mousetrap: any = MousetrapMod;
 
 export default class InputBoxViewController {
 

tsd/extends.d.ts
@@ -24,6 +24,7 @@
  */
 
 /// <reference path="./thrid_party/jquery/jquery.d.ts"/>
+/// <reference path="./thrid_party/mousetrap/mousetrap.d.ts" />
 
 /**
  *  This type defenition provide some additional intafaces which extends exist ones.
@@ -44,4 +45,9 @@ interface JQuery {
     isScrollBottom: () => boolean;
     scrollBottom: () => JQuery;
     sticky: (option?: any) => JQuery;
+}
+
+interface MousetrapStatic {
+    new(element?: Element): MousetrapStatic;
+    prototype: MousetrapStatic;
 }