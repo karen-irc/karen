25b062e9ac43d3000359accb24c18745b37e8a41
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-07-19 21:01:34 +0900

    [client] Port toggle.tpl to React Component.

---
+++ client/script/output/view/ToggleItem.d.ts
@@ -0,0 +1,10 @@
+/// <reference path="../../../../tsd/third_party/react/react.d.ts"/>
+
+import {ComponentClass} from 'react';
+
+interface ToggleItemProps {
+    key?: any;
+    item: any;
+}
+
+export var ToggleItem: ComponentClass<ToggleItemProps>;
