cbd70d1f37e224ea2df88a08a6159cd0695390aa
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-03 06:26:20 +0900

    Revert "Remove the needless reference path for option-t"
    - This reverts commit df91c99f37b1910d8817ee724ab24970286494df.
    - Visual Studio Code v0.8 preview cannot recognize the node modules
      without the reference path.

--- client/script/domain/Network.ts
+++ client/script/domain/Network.ts
@@ -23,6 +23,7 @@
  * THE SOFTWARE.
  */
 
+/// <reference path="../../../node_modules/option-t/option-t.d.ts" />
 /// <reference path="../../../node_modules/rx/ts/rx.all.es6.d.ts" />
 
 import * as Rx from 'rx';
