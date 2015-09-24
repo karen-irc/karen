1a2a4d034750a899fa962cce029574013d4363ba
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-25 06:25:12 +0900

    Make 'Sidebar' a stateless component.

--- server/view/Sidebar.js
+++ server/view/Sidebar.js
@@ -25,22 +25,15 @@
 
 import * as React from 'react';
 
-export class Sidebar extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <aside id='sidebar'>
-                <div className='tse-content'>
-                    <div className='networks'></div>
-                    <div className='empty'>
-                        {'You\'re not connected to any networks yet.'}
-                    </div>
+export function Sidebar() {
+    return (
+        <aside id='sidebar'>
+            <div className='tse-content'>
+                <div className='networks'></div>
+                <div className='empty'>
+                    {'You\'re not connected to any networks yet.'}
                 </div>
-            </aside>
-        );
-    }
+            </div>
+        </aside>
+    );
 }
