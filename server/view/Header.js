9430443ee6a7ebfad42c12867339f4b02289732b
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-25 06:22:03 +0900

    Make 'Header' a stateless component

--- server/view/Header.js
+++ server/view/Header.js
@@ -25,41 +25,31 @@
 
 import * as React from 'react';
 
-export class Header extends React.Component {
+export function Header({ theme }) {
+    return (
+        <head>
+            <meta charSet='utf-8'/>
+            <meta name='viewport' content='width=device-width, user-scalable=no'/>
+            <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
+            <meta name='apple-mobile-web-app-capable' content='yes'/>
+            <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'/>
+            <meta name='mobile-web-app-capable' content='yes'/>
 
-    constructor(props) {
-        super(props);
-    }
+            <title>karen</title>
 
-    render() {
-        const theme = this.props.theme;
+            <link rel='stylesheet' href='css/bootstrap.css'/>
+            <link rel='stylesheet' href='/dist/css/style.css'/>
+            <link id='theme' rel='stylesheet' href={theme}/>
 
-        return (
-            <head>
-                <meta charSet='utf-8'/>
-                <meta name='viewport' content='width=device-width, user-scalable=no'/>
-                <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
-                <meta name='apple-mobile-web-app-capable' content='yes'/>
-                <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'/>
-                <meta name='mobile-web-app-capable' content='yes'/>
+            <link rel='shortcut icon' href='/img/favicon.png'/>
+            <link rel='icon' sizes='192x192' href='/img/touch-icon-192x192.png'/>
+            <link rel='apple-touch-icon' sizes='120x120' href='/img/apple-touch-icon-120x120.png'/>
 
-                <title>karen</title>
-
-                <link rel='stylesheet' href='css/bootstrap.css'/>
-                <link rel='stylesheet' href='/dist/css/style.css'/>
-                <link id='theme' rel='stylesheet' href={theme}/>
-
-                <link rel='shortcut icon' href='/img/favicon.png'/>
-                <link rel='icon' sizes='192x192' href='/img/touch-icon-192x192.png'/>
-                <link rel='apple-touch-icon' sizes='120x120' href='/img/apple-touch-icon-120x120.png'/>
-
-                <script defer='true' src='/dist/js/libs.min.js'></script>
-                <script defer='true' src='/dist/js/karen.js'></script>
-            </head>
-        );
-    }
+            <script defer='true' src='/dist/js/libs.min.js'></script>
+            <script defer='true' src='/dist/js/karen.js'></script>
+        </head>
+    );
 }
-
 Header.propTypes = {
     theme: React.PropTypes.string.isRequired,
 };
