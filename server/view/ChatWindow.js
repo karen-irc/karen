87b4bbba0c12cc50bace30a6dbace1d170fe9b49
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-25 06:32:53 +0900

    Make 'ChatWindow' a stateless component

--- server/view/ChatWindow.js
+++ server/view/ChatWindow.js
@@ -25,15 +25,8 @@
 
 import * as React from 'react';
 
-export class ChatWindow extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div id='chat' className='no-colors'></div>
-        );
-    }
+export function ChatWindow(props) {
+    return (
+        <div id='chat' className='no-colors'></div>
+    );
 }
