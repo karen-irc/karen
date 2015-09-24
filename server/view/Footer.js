312b8371d66f7a8f57c3c5dd1ba022937c1b32c9
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-09-25 06:31:02 +0900

    Make 'Footer' a stateless component.

--- server/view/Footer.js
+++ server/view/Footer.js
@@ -25,28 +25,21 @@
 
 import * as React from 'react';
 
-export class Footer extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <footer id='footer'>
-                <button className='icon sign-in'
-                        data-target='#sign-in'
-                        title='Sign in to karen'></button>
-                <button className='icon connect'
-                        data-target='#connect'
-                        title='Connect to network'></button>
-                <button className='icon settings'
-                        data-target='#settings'
-                        title='Client settings'></button>
-                <button id='sign-out'
-                        className='icon sign-out'
-                        title='Sign out'></button>
-            </footer>
-        );
-    }
+export function Footer(props) {
+    return (
+        <footer id='footer'>
+            <button className='icon sign-in'
+                    data-target='#sign-in'
+                    title='Sign in to karen'></button>
+            <button className='icon connect'
+                    data-target='#connect'
+                    title='Connect to network'></button>
+            <button className='icon settings'
+                    data-target='#settings'
+                    title='Client settings'></button>
+            <button id='sign-out'
+                    className='icon sign-out'
+                    title='Sign out'></button>
+        </footer>
+    );
 }
