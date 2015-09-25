karen-irc/karen

commit 23dcaf0dc6797af1d1a0b6a1386da7c1d8e7d66b

Make 'Layout' a stateless component.

server/view/Layout.js
@@ -25,21 +25,14 @@
 
 import * as React from 'react';
 
-export class Layout extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div id='wrap'>
-                <div id='viewport'>
-                    {this.props.children}
-                </div>
+export function Layout(props) {
+    return (
+        <div id='wrap'>
+            <div id='viewport'>
+                {this.props.children}
             </div>
-        );
-    }
+        </div>
+    );
 }
 Layout.propTypes = {
     // This is an opaque data structure.

commit d15cc3ff9948cadd484b825ace2e32cce1bf3e9e

Fix the regression by 23dcaf0dc6797af1d1a0b6a1386da7c1d8e7d66b

server/view/Layout.js
@@ -29,7 +29,7 @@ export function Layout(props) {
     return (
         <div id='wrap'>
             <div id='viewport'>
-                {this.props.children}
+                {props.children}
             </div>
         </div>
     );