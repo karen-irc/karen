b37672891fe4a4a17cfca1bd1999a9d9c2ffd6fa
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-11-26 16:47:48 +0900

    eslint: Enable 'no-implied-eval'

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,6 +76,7 @@ module.exports = {
         'no-div-regex': 2,
         'no-empty-pattern': 2,
         'no-eq-null': 2,
+        'no-implied-eval': 2,
         'no-invalid-this': 1,
         'no-param-reassign': [1, {
             'props': true
