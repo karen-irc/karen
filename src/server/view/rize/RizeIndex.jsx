b57fef9ecfeb238515740c163019bde37fab10ec
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-01-23 03:11:33 +0900

    feat(Rize): Add the debugging view.

--- src/server/view/rize/RizeIndex.jsx
+++ src/server/view/rize/RizeIndex.jsx
@@ -31,7 +31,9 @@ export function RizeIndex() {
     return (
         <html>
             <RizeHeader/>
-            <body></body>
+            <body>
+                <div id='js-main-content'></div>
+            </body>
         </html>
     );
 }
