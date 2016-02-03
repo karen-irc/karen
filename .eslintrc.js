448f439676836840cc2f1fd99fd11a5d9e818dfc
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-02-03 13:49:00 +0900

    fix(ESLint): Remove obsolated no-empty-label

--- .eslintrc.js
+++ .eslintrc.js
@@ -105,7 +105,6 @@ module.exports = {
         'no-div-regex': 2,
         'no-empty-pattern': 2,
         'no-else-return': 0, // http://eslint.org/docs/rules/no-else-return
-        'no-empty-label': 2, // http://eslint.org/docs/rules/no-empty-label
         'no-eq-null': 2,
         'no-eval': 2,
         'no-extend-native': 2, // http://eslint.org/docs/rules/no-extend-native
