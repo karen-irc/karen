3be298488afa81beed839f1c030ee8fa6bd75335
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-03-04 14:59:51 +0900

    chore(.eslint.js): sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,8 +55,8 @@ module.exports = {
         'no-dupe-args': 2,
         'no-dupe-keys': 2,
         'no-duplicate-case': 2,
-        'no-empty-character-class': 2,
         'no-empty': 2,
+        'no-empty-character-class': 2,
         'no-ex-assign': 2,
         'no-extra-boolean-cast': 0,
         'no-extra-parens': 0,
@@ -101,9 +101,9 @@ module.exports = {
         'no-caller': 2,
         'no-case-declarations': 2,
         'no-div-regex': 2,
+        'no-empty-function': 1, // http://eslint.org/docs/rules/no-empty-function
         'no-empty-pattern': 2,
         'no-else-return': 0, // http://eslint.org/docs/rules/no-else-return
-        'no-empty-function': 1, // http://eslint.org/docs/rules/no-empty-function
         'no-eq-null': 2,
         'no-eval': 2,
         'no-extend-native': 2, // http://eslint.org/docs/rules/no-extend-native
