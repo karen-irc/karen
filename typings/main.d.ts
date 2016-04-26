2f0ff46fcaded82dd87d0b08af1bf42a87bbfdb2
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-04-27 01:40:37 +0900

    chore(typings): introduce node.d.ts

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="main/ambient/cookies-js/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/react-dom/index.d.ts" />
 /// <reference path="main/ambient/react/index.d.ts" />
 /// <reference path="main/ambient/socket.io-client/index.d.ts" />
