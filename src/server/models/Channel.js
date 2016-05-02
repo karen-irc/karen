karen-irc/karen

commit c097f6ce590aa7bf0884d87cac3e6ef405ce7594

refactor(server/model): stop default export

src/server/models/Channel.js
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import ChannelType from './ChannelType';
+import {ChannelType} from './ChannelType';
 
 const MODES = [
     '~',
@@ -34,7 +34,7 @@ const MODES = [
 
 let id = 0;
 
-export default class Channal {
+export class Channal {
 
     /**
      *  @constructor

src/server/Client.js
@@ -1,13 +1,13 @@
 import {Some, None} from 'option-t';
-import ChannelType from './models/ChannelType';
+import {ChannelType} from './models/ChannelType';
 import crypto from 'crypto';
 import fs from 'fs';
 import path from 'path';
 import * as Logger from './Logger';
 import net from 'net';
-import Message from './models/Message';
-import MessageType from './models/MessageType';
-import Network from './models/Network';
+import {Message} from './models/Message';
+import {MessageType} from './models/MessageType';
+import {Network} from './models/Network';
 import slate from 'slate-irc';
 import tls from 'tls';
 import ConfigDriver from './adapter/ConfigDriver';

src/server/models/ChannelType.js
@@ -24,10 +24,8 @@
  */
 
 /** @enum {string} */
-const ChannelType = Object.freeze({
+export const ChannelType = Object.freeze({
     CHANNEL: 'channel',
     LOBBY: 'lobby',
     QUERY: 'query',
 });
-
-export default ChannelType;

src/server/models/Hostmask.js
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  */
 
-export default class Hostmask {
+export class Hostmask {
 
     /**
      *  @constructor

src/server/models/Message.js
@@ -23,7 +23,8 @@
  * THE SOFTWARE.
  */
 import moment from 'moment';
-import MessageType from './MessageType';
+
+import {MessageType} from './MessageType';
 
 let id = 0;
 
@@ -54,7 +55,7 @@ function findUserImage(channel, message) {
     return hostmask.hostname;
 }
 
-export default class Message {
+export class Message {
 
     /**
      *  @constructor

src/server/models/MessageType.js
@@ -24,7 +24,7 @@
  */
 
 /** @enum {string} */
-const MessageType = Object.freeze({
+export const MessageType = Object.freeze({
     ACTION: 'action',
     ERROR: 'error',
     JOIN: 'join',
@@ -40,5 +40,3 @@ const MessageType = Object.freeze({
     TOPIC: 'topic',
     WHOIS: 'whois'
 });
-
-export default MessageType;

src/server/models/Network.js
@@ -23,8 +23,8 @@
  * THE SOFTWARE.
  */
 
-import Channel from './Channel';
-import ChannelType from './ChannelType';
+import {Channel} from './Channel';
+import {ChannelType} from './ChannelType';
 
 /**
  *  @param  {string}    str
@@ -54,7 +54,7 @@ function prettify(host) {
 /** @type   {number}    */
 let id = 0;
 
-export default class Network {
+export class Network {
 
     /**
      *  @constructor

src/server/models/User.js
@@ -23,7 +23,7 @@
  * THE SOFTWARE.
  */
 
-export default class User {
+export class User {
 
     /**
      *  @constructor

commit 79da511b255f3f9e1681bff25a27a96d787785e3

Fix the regression by #636

src/server/models/Channel.js
@@ -34,7 +34,7 @@ const MODES = [
 
 let id = 0;
 
-export class Channal {
+export class Channel {
 
     /**
      *  @constructor