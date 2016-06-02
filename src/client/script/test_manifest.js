39eea23edd2f6f03039bd4c63b39708575d285b9
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-06-02 15:19:14 +0900

    chore(testing): introduce jsdom

--- src/client/script/test_manifest.js
+++ src/client/script/test_manifest.js
@@ -1,1 +1,23 @@
+import {jsdom} from 'jsdom';
+
+const htmlStr = `
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body></body>
+</html>`;
+
+// XXX:
+// Of course, we would not like to extend the global object
+// to create the perfect mock of web browser.
+// However, we also think it would be danger to use auto binding by `Object.assign()` or other similar ways.
+// It might override Node.js's builtin variables.
+// So we add properties by hand.
+global.document = jsdom(htmlStr, {
+    url: 'https://localhost:8080/',
+});
+global.window = global.document.defaultView;
+global.self = global.window; // `window.self`
+
 require('./lib/test/test_ExIterable');
