karen-irc/karen

commit aaf0840e022fa35c1bef18e5e67be39823d24b22

feat(tsconfig): enable 'noUnusedLocals' (#712)
* refactor(client): add RizeClient

* feat(tsconfig): enable 'noUnusedLocals'

src/client/script/rize.ts
@@ -1,0 +1,85 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016 Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
+ * Copyright (c) 2016 Yusuke Suzuki <utatane.tea@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import {AuthRepository} from './adapter/AuthRepository';
+import {CookieDriver} from './adapter/CookieDriver';
+
+import {AppActionCreator} from './intent/action/AppActionCreator';
+import {MessageActionCreator} from './intent/action/MessageActionCreator';
+import {NotificationActionCreator} from './intent/action/NotificationActionCreator';
+import {UIActionCreator} from './intent/action/UIActionCreator';
+
+import {NotificationPresenter} from './output/NotificationPresenter';
+import {WindowPresenter} from './output/WindowPresenter';
+import {SidebarContext} from './output/context/SidebarContext';
+import {AppView} from './output/view/AppView';
+import {InputBoxView} from './output/view/InputBoxView';
+import {MainContentAreaView} from './output/view/MainContentAreaView';
+import {SidebarFooterView} from './output/view/SidebarFooterView';
+
+import {ConfigRepository} from './settings/repository/ConfigRepository';
+import {GeneralSettingContext} from './settings/context/GeneralSettingContext';
+
+export class RizeClient {
+    intent: IntentBundler;
+
+    inputBox: InputBoxView;
+    appWindow: WindowPresenter;
+    appView: AppView;
+    windows: MainContentAreaView;
+    footer: SidebarFooterView;
+    settings: GeneralSettingContext;
+    sidebarView: SidebarContext;
+
+    config: ConfigRepository;
+    auth: AuthRepository;
+    cookie: CookieDriver;
+
+    private _notify: NotificationPresenter;
+
+    constructor() {
+        const intent = new IntentBundler()
+        this.intent = intent;
+
+        const cookie = new CookieDriver();
+        this.cookie = cookie;
+        this.config = new ConfigRepository(cookie);
+        this.auth = new AuthRepository(cookie, intent.app.dispatcher().signout);
+
+        this._notify = new NotificationPresenter(this.config, intent.notify, intent.ui);
+    }
+}
+
+export class IntentBundler {
+    app: AppActionCreator;
+    notify: NotificationActionCreator;
+    ui: UIActionCreator;
+    message: MessageActionCreator;
+
+    constructor() {
+        this.notify = new NotificationActionCreator();
+        this.ui = new UIActionCreator();
+        this.message = new MessageActionCreator();
+    }
+}

src/client/script/adapter/AuthRepository.ts
@@ -25,6 +25,7 @@
  */
 
 import {Option} from 'option-t';
+import {Subject, Subscription} from 'rxjs';
 
 import {CookieDriver} from './CookieDriver';
 
@@ -35,11 +36,18 @@ const KEY_USER = 'user';
 
 export class AuthRepository {
 
-    _cookie: CookieDriver;
+    private _cookie: CookieDriver;
+    private _disposer: Subscription;
 
-    constructor(cookie: CookieDriver) {
-        /** @type {CookieDriver}  */
+    constructor(cookie: CookieDriver, signoutAction: Subject<void>) {
         this._cookie = cookie;
+
+        const disposer = new Subscription();
+        this._disposer = disposer;
+
+        disposer.add(signoutAction.subscribe(() => {
+            this.removeToken();
+        }));
     }
 
     getUser(): Option<string> {

src/client/script/karen.ts
@@ -3,75 +3,66 @@ import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import * as Rx from 'rxjs';
 
-import {AppActionCreator} from './intent/action/AppActionCreator';
+
 import {AppView} from './output/view/AppView';
-import {AuthRepository} from './adapter/AuthRepository';
+
 import {Channel} from './domain/Channel';
 import {ChannelId} from './domain/ChannelDomain';
-import {ConfigRepository} from './settings/repository/ConfigRepository';
-import {CookieDriver} from './adapter/CookieDriver';
 import {DomainState} from './domain/DomainState';
 import {SidebarFooterView} from './output/view/SidebarFooterView';
 import {GeneralSettingContext} from './settings/context/GeneralSettingContext';
 import {InputBoxView} from './output/view/InputBoxView';
 import {MainContentAreaView} from './output/view/MainContentAreaView';
-import {MessageActionCreator} from './intent/action/MessageActionCreator';
 import {MessageGateway} from './adapter/MessageGateway';
 import {MessageList} from './output/view/MessageItem';
-import {NotificationActionCreator} from './intent/action/NotificationActionCreator';
-import {NotificationPresenter} from './output/NotificationPresenter';
 import {SidebarContext} from './output/context/SidebarContext';
 import {SocketIoDriver} from './adapter/SocketIoDriver';
 import {ToggleItem} from './output/view/ToggleItem';
-import {UIActionCreator} from './intent/action/UIActionCreator';
+
 import {WindowPresenter} from './output/WindowPresenter';
 
-declare const momoent: any;
-declare const process: {
-    env: any;
-};
+import {RizeClient} from './rize';
 
-const appAction = new AppActionCreator();
-const messageAction = new MessageActionCreator();
-const notifyAction = new NotificationActionCreator();
-const uiAction = new UIActionCreator();
+declare global {
+    interface Window {
+        gKarenClientApp: RizeClient;
+    }
+}
+window.gKarenClientApp = new RizeClient();
 
 const socket = new SocketIoDriver();
-const messageGateway = new MessageGateway(socket, messageAction);
-const cookie = new CookieDriver();
-const config = new ConfigRepository(cookie);
-/* tslint:disable no-unused-variable */
-const notify = new NotificationPresenter(config, notifyAction, uiAction);
-/* tslint:enable */
-const auth = new AuthRepository(cookie);
+const messageGateway = new MessageGateway(socket, window.gKarenClientApp.intent.message);
 
 document.addEventListener('DOMContentLoaded', function onLoad() {
     document.removeEventListener('DOMContentLoaded', onLoad);
 
+    const intent = window.gKarenClientApp.intent;
+    const notifyAction = intent.notify;
+    const uiAction = intent.ui;
     const globalState = new DomainState(messageGateway, uiAction);
-    /* tslint:disable no-unused-variable */
-    const appWindow = new WindowPresenter(globalState, appAction, notifyAction, uiAction);
-    const appView = new AppView(document.getElementById('viewport')!, uiAction);
-    const windows = new MainContentAreaView(globalState, document.getElementById('windows')!, cookie, messageGateway, messageAction, uiAction);
-    const inputBox = new InputBoxView(globalState, document.getElementById('js-form')!, messageAction, uiAction);
-    const settings = new GeneralSettingContext(config, notifyAction);
-    const sidebarView = new SidebarContext(globalState, messageAction, uiAction);
-    sidebarView.onActivate(document.getElementById('sidebar')!);
-    const footer = new SidebarFooterView(globalState, document.getElementById('footer')!, appAction, uiAction);
-    /* tslint:enable */
-    settings.onActivate(document.getElementById('settings')!);
+    const auth = window.gKarenClientApp.auth;
+
+    window.gKarenClientApp.appWindow = new WindowPresenter(globalState, intent.app, notifyAction, uiAction);
+    window.gKarenClientApp.appView = new AppView(document.getElementById('viewport')!, uiAction);
+    window.gKarenClientApp.windows = new MainContentAreaView(globalState, document.getElementById('windows')!, window.gKarenClientApp.cookie, messageGateway, intent.message, uiAction);
+    window.gKarenClientApp.inputBox = new InputBoxView(globalState, document.getElementById('js-form')!, intent.message, uiAction);
+    window.gKarenClientApp.settings = new GeneralSettingContext(window.gKarenClientApp.config, notifyAction);
+    window.gKarenClientApp.sidebarView = new SidebarContext(globalState, intent.message, uiAction);
+    window.gKarenClientApp.sidebarView.onActivate(document.getElementById('sidebar')!);
+    window.gKarenClientApp.footer = new SidebarFooterView(globalState, document.getElementById('footer')!, intent.app, uiAction);
+    window.gKarenClientApp.settings.onActivate(document.getElementById('settings')!);
 
     const chat = document.getElementById('chat');
 
     messageGateway.disconnected().subscribe(function(){
-        appAction.reload();
+        intent.app.reload();
     });
 
     socket.auth().subscribe(function(_: any) {
         const body: HTMLElement = window.document.body;
         const login = document.getElementById('sign-in');
         if (login === null) {
-            appAction.reload();
+            intent.app.reload();
             return;
         }
         Array.from(login.querySelectorAll('.btn')).forEach(function(element: HTMLInputElement){
@@ -134,7 +125,7 @@ document.addEventListener('DOMContentLoaded', function onLoad() {
         }
     });
 
-    const options = config.get();
+    const options = window.gKarenClientApp.config.get();
 
     socket.toggle().subscribe(function(data) {
         const toggle = document.getElementById('toggle-' + data.id)!;
@@ -158,7 +149,7 @@ document.addEventListener('DOMContentLoaded', function onLoad() {
         }
     });
 
-    config.asObservable().subscribe((settings) => { // FIXME
+    window.gKarenClientApp.config.asObservable().subscribe((settings) => { // FIXME
         const classList = chat!.classList;
         [
             ['join', settings.join],
@@ -247,7 +238,7 @@ document.addEventListener('DOMContentLoaded', function onLoad() {
         });
         if (network.isSome) {
             const nickname = network.unwrap().nickname;
-            inputBox.setNickname(nickname);
+            window.gKarenClientApp.inputBox.setNickname(nickname);
         }
 
         if (screen.width > 768 && target.classList.contains('chan')) {
@@ -269,9 +260,5 @@ document.addEventListener('DOMContentLoaded', function onLoad() {
         evt.initCustomEvent('show', true, true, null);
         target.dispatchEvent(evt);
         (target as HTMLElement).style.zIndex = String(top++);
-    });
-
-    appAction.dispatcher().signout.subscribe(function(){
-        auth.removeToken();
     });
 });

commit 171984c1fb1432befd724c240226740adab877dd

fix the regression by #712

src/client/script/rize.ts
@@ -59,7 +59,7 @@ export class RizeClient {
     private _notify: NotificationPresenter;
 
     constructor() {
-        const intent = new IntentBundler()
+        const intent = new IntentBundler();
         this.intent = intent;
 
         const cookie = new CookieDriver();
@@ -78,6 +78,7 @@ export class IntentBundler {
     message: MessageActionCreator;
 
     constructor() {
+        this.app = new AppActionCreator();
         this.notify = new NotificationActionCreator();
         this.ui = new UIActionCreator();
         this.message = new MessageActionCreator();