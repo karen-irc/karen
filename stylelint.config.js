3a96f67269671fc73a928c5db964c45cdb52f57d
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-08-06 03:48:06 +0900

    lint(style): add some rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,5 +8,18 @@ module.exports = {
         './tools/stylelint/stylelintrc_core.js',
     ],
 
-    'rules': {},
+    // http://stylelint.io/user-guide/rules/
+    'rules': {
+        'at-rule-empty-line-before': null, // FIXME
+        'block-no-single-line': null, // FIXME
+        'color-hex-length': null, // FIXME
+        'declaration-no-important': null, // FIXME
+        'font-family-name-quotes': null, // FIXME
+        'no-descending-specificity': null, // FIXME
+        'selector-no-universal': null, // FIXME
+        'property-no-vendor-prefix': null, // FIXME
+        'selector-no-vendor-prefix': null, // FIXME
+        'selector-pseudo-element-colon-notation': null, // FIXME
+        'string-quotes': null, // FIXME
+    },
 };
