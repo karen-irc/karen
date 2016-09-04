6126c5ce592e2dda6d241997a35552c04d01701d
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-05 00:29:34 +0900

    build(rollup): enable babel transform

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
 
 const isRelease = process.env.NODE_ENV === 'production';
 
@@ -15,6 +16,42 @@ assert.ok(!!KAREN_ENTRY_POINT, 'not found process.env.KAREN_ENTRY_POINT');
 
 const KAREN_CLIENT_DIST_DIR = process.env.KAREN_CLIENT_DIST_DIR;
 assert.ok(!!KAREN_CLIENT_DIST_DIR, 'not found process.env.KAREN_CLIENT_DIST_DIR');
+
+const babelPresets = [
+];
+
+let babelPlugins = [
+    // For our target browsers, we need not some transforms.
+
+    // es2016 level
+    'babel-plugin-transform-exponentiation-operator',
+    // es2017 level
+    'babel-plugin-syntax-trailing-function-commas',
+    'babel-plugin-transform-async-to-generator',
+
+    // for React
+    'syntax-jsx',
+    'transform-react-jsx',
+
+    // some utilitis
+    'transform-inline-environment-variables',
+    'transform-node-env-inline',
+
+    // to remove duplicated helper methods inserted by babel.
+    'external-helpers',
+];
+
+if (isRelease) {
+    babelPlugins = babelPlugins.concat([
+        'transform-react-constant-elements',
+        'transform-react-inline-elements',
+    ]);
+}
+else {
+    babelPlugins = babelPlugins.concat([
+        'transform-react-jsx-source',
+    ]);
+}
 
 // https://github.com/rollup/rollup/wiki/JavaScript-API
 // https://github.com/rollup/rollup/wiki/Command-Line-Interface
@@ -44,6 +81,13 @@ module.exports = {
         commonjs({
             include: 'node_modules/**',
             ignoreGlobal: true,
+        }),
+
+        // https://github.com/rollup/rollup-plugin-babel
+        babel({
+            exclude: 'node_modules/**',
+            presets: babelPresets,
+            plugins: babelPlugins,
         }),
     ]
 };
