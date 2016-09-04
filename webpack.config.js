daa4a84c6255fad879b71259376bf5d2c8f07d0a
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-05 01:10:20 +0900

    build(babel): use webpack 2's tree shaking

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ let babelPlugins = [
     'check-es2015-constants',
     'transform-es2015-destructuring',
     'transform-es2015-function-name',
-    'transform-es2015-modules-commonjs',
     'transform-es2015-object-super',
     'transform-es2015-parameters',
     'transform-es2015-spread',
