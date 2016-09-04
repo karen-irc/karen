97a5faa780687eac1410a000ed9f925c54917e47
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-05 00:08:44 +0900

    build(rollup): enable to import commonjs installed by npm

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@
 const assert = require('assert');
 const path = require('path');
 
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+
 const isRelease = process.env.NODE_ENV === 'production';
 
 const KAREN_ENTRY_POINT = process.env.KAREN_ENTRY_POINT;
@@ -24,5 +27,19 @@ module.exports = {
     sourceMap: true,
     treeshake: true,
 
-    plugins: []
+    plugins: [
+        // https://github.com/rollup/rollup-plugin-node-resolve
+        nodeResolve({
+            module: true,
+            main: true,
+            browser: true, // for browser
+            preferBuiltins: false, // linking for browser
+        }),
+
+        // https://github.com/rollup/rollup-plugin-commonjs
+        commonjs({
+            include: 'node_modules/**',
+            ignoreGlobal: true,
+        }),
+    ]
 };
