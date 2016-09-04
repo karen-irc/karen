b35b282307448b8f7aeeb8ed5946a0e36c093fc5
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-05 04:55:39 +0900

    feat(build): add closure compiler js as a minifier

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@
 
 const assert = require('assert');
 
+const ClosureCompiler = require('google-closure-compiler-js').webpack;
+
 const isRelease = process.env.NODE_ENV === 'production';
 
 const KAREN_ENTRY_POINT = process.env.KAREN_ENTRY_POINT;
@@ -59,6 +61,24 @@ else {
     ]);
 }
 
+const webpackPlugins = [];
+if (isRelease) {
+    // https://github.com/google/closure-compiler-js
+    const closure = new ClosureCompiler({
+        options: {
+            languageIn: 'ECMASCRIPT6',
+            languageOut: 'ECMASCRIPT5_STRICT',
+            compilationLevel: 'SIMPLE',
+            warningLevel: 'VERBOSE',
+
+            createSourceMap: true,
+            rewritePolyfills: false,
+            useTypesForOptimization: false,
+        },
+    });
+    webpackPlugins.push(closure);
+}
+
 module.exports = {
     bail: true,
 
@@ -98,4 +118,6 @@ module.exports = {
             }
         ]
     },
+
+    plugins: webpackPlugins,
 };
