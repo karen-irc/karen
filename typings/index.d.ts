ad73a1bc8a0b2048c96cb65d898c08054382c2c0
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-09-06 03:17:58 +0900

    chore(typings): use the d.ts loading builtin tsc.

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/socket.io-client/index.d.ts" />
-/// <reference path="globals/whatwg-fetch/index.d.ts" />
