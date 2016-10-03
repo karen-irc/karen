24ace19ff84961120011c890e0b4a29b3fdf3be5
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-10-04 00:50:16 +0900

    refactor(build): generate `.babelrc` just in compile time.

---
+++ tools/build/babelrc_for_server.js
@@ -0,0 +1,42 @@
+/*eslint quote-props: [2, "always"] */
+
+'use strict';
+
+// This object is encoded to `.babelrc` with using `JSON.stringify()`.
+module.exports = {
+    'presets': [
+    ],
+
+    'plugins': [
+        // For Node.js v6~, we need not some transforms.
+        'transform-es2015-modules-commonjs',
+
+        // es2016 level
+        'babel-plugin-transform-exponentiation-operator',
+        // es2017 level
+        'babel-plugin-syntax-trailing-function-commas',
+        'babel-plugin-transform-async-to-generator',
+
+        // for React
+        'syntax-jsx',
+        'transform-react-jsx',
+    ],
+
+    'env': {
+        'development': {
+            'plugins': [
+                'transform-react-constant-elements',
+                'transform-react-inline-elements',
+            ],
+        },
+
+        'production': {
+            'plugins': [
+                'transform-react-jsx-source',
+            ],
+        },
+    },
+
+    'parserOpts': {},
+    'generatorOpts': {}
+};
